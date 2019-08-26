1087ee4ad07e16b53cf76e698e0a34889cf4d6af
Kyle Chesney <kyle.2493@gmail.com>
2019-08-26 16:56:05 -0400

    popup menu, populate select with other events set up for using AJAX to change SQL

--- ExternalModule.php
+++ ExternalModule.php
@@ -8,10 +8,20 @@ use REDCap;
 class ExternalModule extends AbstractExternalModule {
 
     function redcap_every_page_top($project_id) {
+        echo ('<script> var ajaxpage = "' . __DIR__ . '/migratedata.php" </script>');
+        include('div.html');
         $this->includeJs('js/mdoe.js');
     }
 
     function includeJs($path) {
         echo '<script src="' . $this->getUrl($path) . '">';
+    }
+
+    function moveEvent() {
+    $new_id = 127;
+    $sql = "UPDATE redcap_data SET event_id = " . $new_id . " WHERE AND project_id = '" . PROJECT_ID .
+    "AND event_id = 126";
+    print_r($sql);
+    print_r("attempt to moveForm");
     }
 }
--- js/mdoe.js
+++ js/mdoe.js
@@ -1,45 +1,87 @@
-// TODO: pick font-awesome icon for button
-const formButton = $( '<input type="image" value="icon" />' );
+let formButton = $( '<i class="fas fa-truck" type="image" style="padding: 5px;"/>' );
+const eFormButton = formButton.clone().addClass('mdoe-event');
+const fFormButton = formButton.clone().addClass('mdoe-form');
+
 document.addEventListener('DOMContentLoaded', function() {
     var events;
     var links;
 
     eventColumns = $('.evGridHdr');
-    links = $('#event_grid_table').children('tbody').find('a');
-
-    console.log(eventColumns);
-    console.log(links);
+    formLinks = $('#event_grid_table').children('tbody').find('a');
 
     $.each(eventColumns, function(i, eventColumn) { 
             // $element.appendTo(formButton); does not work, only appears on last element
-            formButton.clone().appendTo(eventColumn);
+            eFormButton.clone().appendTo(eventColumn);
             });
 
-    $.each(links, function(i, link) {
+    $.each(formLinks, function(i, link) {
             // $element.prependTo(link); does not work, only appears on last element
-            formButton.clone().prependTo(link);
+            fFormButton.clone().insertBefore(link);
             });
 
+    console.log(formLinks);
 
-    dialog = $( "dialog-form" ).dialog({
+    dialogForm = $( "#dialog-mdoe" ).dialog({
       autoOpen: false,
-      height: 400,
-      width: 350,
+      draggable: true,
+      resizable: true,
       modal: true,
       buttons: {
-        "Create an account": console.log,
+        "Migrate Event Data": function() { console.log($(this).find('option')); sendAjaxForEvent('hello world'); },
         Cancel: function() {
-          dialog.dialog( "close" );
+          $(this).dialog( "close" );
         }
       },
-      close: function() {
-        form[ 0 ].reset();
-        allFields.removeClass( "ui-state-error" );
-      }
     });
 
-    function deliverPopupMenu() {
-        return;
-    }
+    $( ".mdoe-event" ).on( "click", function() {
+        let colIndex = $(this).parent().index();
+
+        let otherCols = $( "#event_grid_table thead tr th" ).toArray();
+        otherCols.splice(colIndex, 1); // Remove this event column
+        otherCols.shift(); // Remove form label column
+
+        let titles = {}; // title name : event_id
+        otherCols.forEach( element => titles[element.children[0].textContent] = element.children[1].className.split('evGridHdrInstance-')[1].split(' ', 1)[0] );
+
+        $(dialogForm).find('option').remove();
+        for ( let [key,value] of Object.entries(titles) ) {
+            $('#mdoe-select').append(`<option value="${value}">${key}</option>`);
+            }
+
+        console.log(dialogForm);
+
+
+        let thisColValues = $( `#event_grid_table tbody tr td:nth-child(${colIndex+1})` );
+        thisColValues.css("background-color", "#ff9933");
+
+        //$("#dialog-mdoe")
+        dialogForm
+            .on('dialogclose', function(event) { thisColValues.css("background-color", ""); })
+            .dialog( "open" );
+    });
+
+    $( ".mdoe-form" ).on( "click", function() {
+        console.log('this');
+        console.log(this);
+        console.log( $("#dialog-mdoe") );
+        let page = $(this).next().attr('href')
+                    .split('page=')[1].split('&')[0];
+        let thisRow = $(this).parent().parent('tr');
+        console.log(page);
+        console.log(thisRow);
+        let movableEvents = thisRow.children();
+        console.log(movableEvents);
+        $("#dialog-mdoe").dialog( "open" );
+    });
 
 });
+
+function sendAjaxForEvent(str) {
+    //console.log(str);
+    var ajax = new XMLHttpRequest();
+    ajax.onreadystatechange = function() { console.log(this.responseText)};
+    ajax.open("GET", ajaxpage + "?q=" + str, true);
+    //ajax.open("GET", "migratedata.php?q=" + str, true);
+    ajax.send();
+}
